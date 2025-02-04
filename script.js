7ea86915371d62c691bbdc465a8f7fa3d994d8b7
danielvidal <danielvidal>
2025-02-04 18:53:33 -0300

    acrescentando menu

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function showPage(pageId) {
-    // Hide all pages
     const pages = document.querySelectorAll('.page');
     pages.forEach(page => page.classList.add('hidden'));
 
-    // Show the selected page
     const selectedPage = document.getElementById(pageId);
     if (selectedPage) {
         selectedPage.classList.remove('hidden');
